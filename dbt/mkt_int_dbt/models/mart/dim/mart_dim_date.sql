74dfdb0824fb78a3e57a8eaabc739b45a0c19514
drpdiego <drp_diego@hotmail.com>
2026-01-05 20:25:25 -0300

    Alter mart_dim_date sql code

--- dbt/mkt_int_dbt/models/mart/dim/mart_dim_date.sql
+++ dbt/mkt_int_dbt/models/mart/dim/mart_dim_date.sql
@@ -3,7 +3,7 @@
 ) }}
 
 select 
-    date_day,
+	date_day,
 	day_of_week,
 	day_of_week_name,
 	day_of_week_name_short,
@@ -11,6 +11,9 @@ select
 	month_of_year,
 	month_name,
 	month_name_short,
+	cast(right(year_number::text, 2) as integer) as year_number_short,
+    month_name_short || '-' || cast(right(year_number::text, 2) as integer) as month_year_short,
+	case when month_of_year <= 9 then cast(year_number::text || '0' || month_of_year::text as integer) else cast(year_number::text || month_of_year::text as integer) end as year_month_number, 
 	quarter_of_year,
 	year_number
 from {{ ref('stg_date') }}
